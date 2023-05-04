739bc4f774097f7bc989234589a7e44380ea36e6
t1geryan <sgta1980@gmail.com>
2023-05-04 21:19:55 +0300

    code refactoring (delete empty brackets)

--- app/src/main/java/com/example/weatherapp/presenter/contract/ScreenContainer.kt
+++ app/src/main/java/com/example/weatherapp/presenter/contract/ScreenContainer.kt
@@ -4,5 +4,4 @@ package com.example.weatherapp.presenter.contract
  * if a class (e.g. Fragment) implements this interface,
  * it means that it only serves to store other screens and is not displayed on the screen
  */
-interface ScreenContainer {
-}
+interface ScreenContainer
