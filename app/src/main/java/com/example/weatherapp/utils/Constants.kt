2cac7ca979ca37f6bbd9d9e2c05048ec89139141
t1geryan <sgta1980@gmail.com>
2023-05-12 20:16:53 +0300

    change OPEN_WEATHER_API key

--- app/src/main/java/com/example/weatherapp/utils/Constants.kt
+++ app/src/main/java/com/example/weatherapp/utils/Constants.kt
@@ -3,5 +3,5 @@ package com.example.weatherapp.utils
 object Constants {
 
     const val MAP_API_KEY = "a122b20c-3546-43ca-8092-560ed12702f6"
-    const val OPEN_WEATHER_API_KEY = "9af79da0f71bd7fbf12d4e83a3ca3d1c"
+    const val OPEN_WEATHER_API_KEY = "0fd028a9364ad11692c9397667b1414f"
 }
