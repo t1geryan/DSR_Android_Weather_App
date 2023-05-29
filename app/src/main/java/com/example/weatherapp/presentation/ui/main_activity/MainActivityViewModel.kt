9464ee6fecbf07ea5586d82702acf09f3a3d8ef1
t1geryan <sgta1980@gmail.com>
2023-05-29 19:02:27 +0300

    optimize imports

--- app/src/main/java/com/example/weatherapp/presentation/ui/main_activity/MainActivityViewModel.kt
+++ app/src/main/java/com/example/weatherapp/presentation/ui/main_activity/MainActivityViewModel.kt
@@ -7,7 +7,9 @@ import com.example.weatherapp.domain.repositories.SettingsRepository
 import com.example.weatherapp.presentation.ui_utils.tryEmitFlow
 import com.example.weatherapp.presentation.ui_utils.viewModelScopeIO
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
