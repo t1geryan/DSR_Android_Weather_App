088bfc5002152e970577c72ca1c01002046990d1
t1geryan <sgta1980@gmail.com>
2023-05-29 19:02:01 +0300

    fix a bug in displaying the wrong metrics and theme system on the first launch of the application (default settings were shown instead of those set until the user made changes)
    change SharedFlow to StateFlow in MainActivityViewModel.kt

--- app/src/main/java/com/example/weatherapp/presentation/ui/main_activity/MainActivityViewModel.kt
+++ app/src/main/java/com/example/weatherapp/presentation/ui/main_activity/MainActivityViewModel.kt
@@ -7,9 +7,7 @@ import com.example.weatherapp.domain.repositories.SettingsRepository
 import com.example.weatherapp.presentation.ui_utils.tryEmitFlow
 import com.example.weatherapp.presentation.ui_utils.viewModelScopeIO
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,13 +16,13 @@ class MainActivityViewModel @Inject constructor(
     private val settingsRepository: SettingsRepository
 ) : ViewModel() {
 
-    private val _unitsSystemSetting = MutableSharedFlow<AppUnitsSystem>()
-    val unitsSystemSetting: SharedFlow<AppUnitsSystem>
-        get() = _unitsSystemSetting.asSharedFlow()
+    private val _unitsSystemSetting = MutableStateFlow(AppUnitsSystem.METRIC_SYSTEM)
+    val unitsSystemSetting: StateFlow<AppUnitsSystem>
+        get() = _unitsSystemSetting.asStateFlow()
 
-    private val _appThemeSetting = MutableSharedFlow<AppTheme>()
-    val appThemeSetting: SharedFlow<AppTheme>
-        get() = _appThemeSetting.asSharedFlow()
+    private val _appThemeSetting = MutableStateFlow(AppTheme.SYSTEM_THEME)
+    val appThemeSetting: StateFlow<AppTheme>
+        get() = _appThemeSetting.asStateFlow()
 
     init {
         fetchUnitsSystemSetting()
@@ -34,7 +32,7 @@ class MainActivityViewModel @Inject constructor(
     private fun fetchUnitsSystemSetting() {
         tryEmitFlow {
             settingsRepository.getCurrentUnitsSystem().collect {
-                _unitsSystemSetting.emit(it)
+                _unitsSystemSetting.value = it
             }
         }
     }
@@ -42,7 +40,7 @@ class MainActivityViewModel @Inject constructor(
     private fun fetchAppThemeSetting() {
         tryEmitFlow {
             settingsRepository.getCurrentAppTheme().collect {
-                _appThemeSetting.emit(it)
+                _appThemeSetting.value = it
             }
         }
     }
